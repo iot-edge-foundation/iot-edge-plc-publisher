20307359b243dc3e1108ab0a3410592e69c49640
Kevin BEAUGRAND <9513635+kbeaugrand@users.noreply.github.com>
2022-09-19 08:50:23 +0200

    Add json transformer to twin definition

--- src/PlcPublisher/modules/PLC-Publisher/TwinSettings/TagTwinDefinition.cs
+++ src/PlcPublisher/modules/PLC-Publisher/TwinSettings/TagTwinDefinition.cs
@@ -1,8 +1,12 @@
 
+using Newtonsoft.Json.Linq;
+
 namespace PLCPublisher.TwinSettings
 {
     public class TagTwinDefinition : TagDefinition
     {
         public int PollingInterval { get; set; }
+
+        public JObject Transform { get; set; }
     }
 }
